623b012b4b9d49c3dae2080eb710d58452b5af19
DESKTOP-KQT7VQT\tem <784637485@qq.com>
2019-12-13 11:48:19 +0800

    tjp --改配置

--- environments/dev/common/config/params-local.php
+++ environments/dev/common/config/params-local.php
@@ -1,3 +1,16 @@
 <?php
 return [
+    "domains"     => [
+        "static"    => [
+            "cdn"    => [
+                "switch"       => false,
+                "domain"       => "http://static.dev.hsh568.cn",
+                "domain_https" => "https://static.dev.hsh568.cn"
+            ],
+            "domain" => "http://static.dev.hsh568.cn"
+        ],
+        "www"       => [
+            "domain" => "http://www.dev.hsh123.cn"
+        ],
+    ],
 ];
--- environments/prod/common/config/params-local.php
+++ environments/prod/common/config/params-local.php
@@ -1,3 +1,16 @@
 <?php
 return [
+    "domains"     => [
+        "static"    => [
+            "cdn"    => [
+                "switch"       => false,
+                "domain"       => "http://static.test.hsh568.cn",
+                "domain_https" => "https://static.test.hsh568.cn"
+            ],
+            "domain" => "http://static.test.hsh568.cn"
+        ],
+        "www"       => [
+            "domain" => "http://www.test.hsh123.cn"
+        ],
+    ],
 ];
