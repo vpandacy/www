86f180a07e35677be80e2ce70007cbdc070b1ba2
DESKTOP-KQT7VQT\tem <784637485@qq.com>
2019-12-17 15:08:45 +0800

    tjp --不知道怎么往下做了、

--- www/controllers/PageController.php
+++ www/controllers/PageController.php
@@ -6,19 +6,35 @@ namespace www\controllers;
 use common\components\BaseWebController;
 use common\components\HttpClient;
 use common\services\GlobalUrlService;
+use http\Encoding\Stream\Debrotli;
 
 class PageController extends BaseWebController
 {
     public function actionIndex()
     {
+        $this->layout = false;
         $media_id = $this->get('media_id');
         $project_id = $this->get('project_id');
+        $token = md5($project_id . $media_id);
         $info = HttpClient::post(GlobalUrlService::buildApiUrl('/lianzhan/page/index'), [
-            "media_id" => $media_id, "project_id" => $project_id
+            "media_id" => $media_id,
+            "project_id" => $project_id,
+            'token' => $token
         ]);
-        var_dump($this->getCookie('hsh_api'));
-//        var_dump($info);
-        die;
-        return $this->render('index');
+        $info_tmp = json_decode($info , true);
+        if (!$info_tmp['data']){
+            return $this->render('/error/index', ['msg' => $info_tmp['msg']]);
+        }
+        $host = \Yii::$app->params['domains']['cdn_hsh']['domain'];
+        $data = json_decode($info_tmp['data'], true);
+        $page_script =  $data['page_script'];
+        $cdn = $host . "/shophtml/" . $data['project_pinyin'] . "/item.html";
+        $content = HttpClient::get($cdn);
+
+
+        return $this->render('index', [
+            'content' => $content,
+            'page_script' => $page_script
+        ]);
     }
 }
