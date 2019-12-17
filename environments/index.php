3cff7d5955fc8f6fb1820e3d06e59bb19bbfa1b9
DESKTOP-KQT7VQT\tem <784637485@qq.com>
2019-12-17 17:42:53 +0800

    tjp --修改配置环境index.php

--- environments/index.php
+++ environments/index.php
@@ -32,34 +32,29 @@ return [
     'Development' => [
         'path' => 'dev',
         'setWritable' => [
-            'backend/runtime',
-            'backend/web/assets',
-            'frontend/runtime',
-            'frontend/web/assets',
+            'www/runtime',
+            'www/web/assets',
+
         ],
         'setExecutable' => [
             'yii',
             'yii_test',
         ],
         'setCookieValidationKey' => [
-            'backend/config/main-local.php',
-            'frontend/config/main-local.php',
+            'www/config/main-local.php',
         ],
     ],
     'Production' => [
         'path' => 'prod',
         'setWritable' => [
-            'backend/runtime',
-            'backend/web/assets',
-            'frontend/runtime',
-            'frontend/web/assets',
+            'www/runtime',
+            'www/web/assets',
         ],
         'setExecutable' => [
             'yii',
         ],
         'setCookieValidationKey' => [
-            'backend/config/main-local.php',
-            'frontend/config/main-local.php',
+            'www/config/main-local.php',
         ],
     ],
 ];
