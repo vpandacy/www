4cf33cc03f38519e9a36cd7fd5f8f0b258b12fc8
wuwulong <1191540273@qq.com>
2019-12-31 09:13:55 +0800

    修改请求page方法

--- www/controllers/PageController.php
+++ www/controllers/PageController.php
@@ -7,29 +7,29 @@ use common\components\BaseWebController;
 use common\components\helper\DateHelper;
 use common\components\HttpClient;
 use common\services\GlobalUrlService;
-
+use www\services\ApiRequestService;
 
 class PageController extends BaseWebController
 {
     public function actionIndex()
     {
         $this->layout = false;
-        $cookies = "switch_version=dev_20191210_web_template;";
-        HttpClient::setCookie($cookies);
+        $cookies = "switch_version=server;";
         $media_id = $this->get('media_id');
         $project_id = $this->get('project_id');
         $token = md5($project_id . $media_id);
 
-        $info = HttpClient::post(GlobalUrlService::buildApiUrl('/lianzhan/page/index'), [
+        ApiRequestService::setCookies($cookies);
+        $info = ApiRequestService::sendPostRequest('/lianzhan/page/index',[
             "media_id" => $media_id,
             "project_id" => $project_id,
             'token' => $token
         ]);
         if (!$info) {
-            return $this->render('/error/index', ['msg' => '返回信息为空']);
+            return $this->render('/error/index', ['msg' => ApiRequestService::getLastErrorMsg()]);
         }
 
-        $info_tmp = json_decode($info, true);
+        $info_tmp = $info;
         if (!$info_tmp['data']) {
             return $this->render('/error/index', ['msg' => $info_tmp['msg']]);
         }
@@ -38,8 +38,6 @@ class PageController extends BaseWebController
         $page_script = $data['page_script'];
         $cdn = $host . "/shophtml/" . $data['project_pinyin'] . "/item.html";
         $content = HttpClient::get($cdn);
-
-
 
 
         return $this->render('index', [
