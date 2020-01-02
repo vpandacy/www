1f42f8d682b9585fd4c4b28b9ecd4de2509ed020
ymm <1160847953@qq.com>
2020-01-02 14:08:27 +0800

    模板页

--- www/controllers/CodeController.php
+++ www/controllers/CodeController.php
@@ -6,6 +6,7 @@ namespace www\controllers;
 use common\components\BaseWebController;
 use common\components\HttpClient;
 use common\services\GlobalUrlService;
+use www\services\ApiRequestService;
 
 class CodeController extends BaseWebController
 {
@@ -16,31 +17,45 @@ class CodeController extends BaseWebController
         $this->layout = false;
         $cookies = "switch_version=dev_20191210_web_template;";
         //HttpClient::setCookie($cookies);
+        //ApiRequestService::setCookies($cookies);
         $code = $this->get('code');
 
-        $info = HttpClient::post(GlobalUrlService::buildApiUrl('/lianzhan/code/index'), [
+        $info = ApiRequestService::sendPostRequest('/lianzhan/code/index',[
             "code" => $code
         ]);
 
 
         if (!$info) {
-            return $this->render('/error/index', ['msg' => '返回信息为空']);
+            return $this->render('/error/index', ['msg' => ApiRequestService::getLastErrorMsg()]);
         }
 
-        $info_tmp = json_decode($info, true);
+        $info_tmp = $info;
 
         if (!$info_tmp['data']) {
-            return $this->render('/error/index', ['msg' => $info_tmp['msg']]);
+            return $this->render('/error/index', ['msg' => '返回参数不正确']);
         }
         $host = \Yii::$app->params['domains']['cdn_hsh']['domain'];
         $data = json_decode($info_tmp['data'], true);
+        if (!$data){
+            return $this->render('/error/index', ['msg' => '返回信息有误']);
+        }
         $page_script = $data['page_script'];
         $cdn = $host . "/shophtml/" . $data['project_pinyin'] . "/item.html";
         $content = HttpClient::get($cdn);
+        //$data['head_type'] = '2';
+        $head_type = $data['head_type'] ?? 0;
+        switch ($head_type){
+            case 1:
+                $view = 'index1';
+                break;
+            case 2:
+                $view = 'index2';
+                break;
+            default :
+                $view = 'index';
+        }
 
-        $view_type = '';
-
-        return $this->render('index', [
+        return $this->render($view, [
             'content' => $content,
             'page_script' => $page_script,
         ]);
