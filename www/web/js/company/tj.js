a57ae45955b5fdbdacb4697ae7de80124394f2fc
DESKTOP-KQT7VQT\tem <784637485@qq.com>
2020-01-07 09:18:51 +0800

    tjp --完成分类静态页

---
+++ www/web/js/company/tj.js
@@ -0,0 +1,8 @@
+// 百度统计，su.2016-8-23
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?d6f8ce620b724e23caf315f1d829b32f";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
