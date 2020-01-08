7042a7d80907e20a60efb81d6711f8aa72eb86d1
DESKTOP-KQT7VQT\tem <784637485@qq.com>
2020-01-08 09:54:34 +0800

    tjp --分类页面完成

--- www/config/router.php
+++ www/config/router.php
@@ -6,11 +6,12 @@ return [
     'rules' => [
         '<controller:(code)>/<code:[a-zA-Z0-9\-_]+>' => '<controller>/index',
         '<controller:(lybook)>/<code:[a-zA-Z0-9\-_]+>' => '<controller>/index',
+        '<controller:(category)>/<id:\d+>' => '<controller>/index',
+        '<controller:(category)>/<id:\d+>/sort/<sort:\w+>' => '<controller>/index',
         '<action:\w+>'=>'company/<action>',
         '/<controller:\w+>/<action:\w+>' => '<controller>/<action>',
         '<controller:(page)>/<media_id:\d+>/info/<project_id:\d+>' => '<controller>/index',
         '/' => 'default/index',
-        '<controller:(category)>/<id:\d+>' => '<controller>/index',
 
     ],
 ];
