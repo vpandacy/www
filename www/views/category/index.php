41512b4e69ad3f7ae8e5140d646d1348e6004aa6
DESKTOP-KQT7VQT\tem <784637485@qq.com>
2020-01-08 14:47:13 +0800

    tjp --改成不用kfkf_id了

--- www/views/category/index.php
+++ www/views/category/index.php
@@ -327,7 +327,7 @@ use common\services\GlobalUrlService;
                             </div>
                             <div class="l_btn">
                                 <a href="<?= "/code/{$project['code']}" ?>" target="_blank">查看详情</a>
-                                <a href="<?= "https://tb.53kf.com/code/client/{$project['projid']}/{$project['kf_id']}" ?>"
+                                <a href="<?= GlobalUrlService::buildWWWUrl("/code/" . $project['code']) ?>"
                                    target="_blank" class="active">立即咨询</a>
                                 <div class="clearfix"></div>
                             </div>
