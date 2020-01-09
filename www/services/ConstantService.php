7ed524161945a750156a9ca4e79d8b599ace04a6
wuwulong <1191540273@qq.com>
2020-01-09 13:45:47 +0800

    弄回被删除的constant

---
+++ www/services/ConstantService.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hsh
+ * Date: 2020/1/9
+ * Time: 13:44
+ */
+
+namespace www\services;
+
+
+class ConstantService
+{
+    const WEBPAGE_HEAD = 2;
+    const WEBPAGE_ENEME = 1;
+}
