477d0eabad4052338af4df4a4ab6207e25520894
ymm <1160847953@qq.com>
2020-01-09 17:48:15 +0800

    修改搜索页导航

--- www/views/search/index.php
+++ www/views/search/index.php
@@ -93,151 +93,161 @@ use common\services\GlobalUrlService;
 <div id="nav">
     <div class="navBox">
         <ul>
-            <li><a href="/">首页</a></li>
-            <li><a href="/l51/1">特色餐饮</a></li>
-            <li><a href="/l51/7">环保机械</a></li>
-            <li><a href="/l51/6">家居建材</a></li>
-            <li><a href="/l51/5">服装鞋帽</a></li>
-            <li><a href="/l51/138">集成墙</a></li>
-            <li><a href="/l51/35">生活服务</a></li>
-            <li><a href="/l51/8">母婴教育</a></li>
-            <li><a href="/l51/95">休闲食品</a></li>
-            <li><a href="/l51/70">农村致富</a></li>
+            <li>
+                <a href="/">首页</a></li>
+            <li>
+                <a href="">特色餐饮</a></li>
+            <li>
+                <a href="/category/1">环保机械</a></li>
+            <li>
+                <a href="/category/2">家居建材</a></li>
+            <li>
+                <a href="/category/3">服装鞋帽</a></li>
+            <li>
+                <a href="/category/4">集成墙</a></li>
+            <li>
+                <a href="/category/5">生活服务</a></li>
+            <li>
+                <a href="/category/6">母婴教育</a></li>
+            <li><a href="/category/7">休闲食品</a></li>
+            <li><a href="/category/8">农村致富</a></li>
             <li class="last"></li>
+
+
         </ul>
         <div class="max_type">
             <div class="typeBox">
                 <dl>
                     <dt>
-                        <i></i>快餐加盟
+                        <i>
+                        </i>快餐加盟
                     </dt>
                     <dd>
-                        <a href="/l51/97" target="_blank">西式快餐</a>
-                        <a href="/l51/90" target="_blank">熟食加盟</a>
-                        <a href="/l51/40" target="_blank">中式快餐</a>
-                        <a href="/l51/39" target="_blank">韩式快餐</a>
-                    </dd>
+                        <a href="/category/97" target="_blank">西式快餐</a>
+                        <a href="/category/90" target="_blank">熟食加盟</a>
+                        <a href="/category/40" target="_blank">中式快餐</a>
+                        <a href="/category/39" target="_blank">韩式快餐</a></dd>
                 </dl>
                 <dl>
                     <dt>
-                        <i></i>休闲食品
+                        <i>
+                        </i>休闲食品
                     </dt>
                     <dd>
-                        <a href="/l51/96" target="_blank">比萨加盟</a>
-                        <a href="/l51/91    " target="_blank">甜冰淇淋</a>
-                        <a href="/l51/93" target="_blank">甜品加盟</a>
-                        <a href="/l51/136" target="_blank">麻辣烫</a>
-                    </dd>
+                        <a href="/category/96" target="_blank">比萨加盟</a>
+                        <a href="/category/91	" target="_blank">甜冰淇淋</a>
+                        <a href="/category/93" target="_blank">甜品加盟</a>
+                        <a href="/category/136" target="_blank">麻辣烫</a></dd>
                 </dl>
                 <dl>
                     <dt>
-                        <i></i>特色餐饮
+                        <i>
+                        </i>特色餐饮
                     </dt>
                     <dd>
-                        <a href="/l51/98" target="_blank">五谷磨坊</a>
-                        <a href="/l51/42" target="_blank">烤鸡烤鸭</a>
-                        <a href="/l51/41" target="_blank">特色烧烤</a>
-                        <a href="/l51/13" target="_blank">特色小吃</a>
-                    </dd>
+                        <a href="/category/98" target="_blank">五谷磨坊</a>
+                        <a href="/category/42" target="_blank">烤鸡烤鸭</a>
+                        <a href="/category/41" target="_blank">特色烧烤</a>
+                        <a href="/category/13" target="_blank">特色小吃</a></dd>
                 </dl>
                 <dl>
                     <dt>
-                        <i></i>酒水加盟
+                        <i>
+                        </i>酒水加盟
                     </dt>
                     <dd>
-                        <a href="/l51/95" target="_blank">红酒加盟</a>
-                        <a href="/l51/94" target="_blank">白酒加盟</a>
-                        <a href="/l51/92" target="_blank">奶茶加盟</a>
-                        <a href="/l51/37" target="_blank">饮料加盟</a>
-                    </dd>
+                        <a href="/category/95" target="_blank">红酒加盟</a>
+                        <a href="/category/94" target="_blank">白酒加盟</a>
+                        <a href="/category/92" target="_blank">奶茶加盟</a>
+                        <a href="/category/37" target="_blank">饮料加盟</a></dd>
                 </dl>
                 <dl>
                     <dt>
-                        <i></i>饰品礼品
+                        <i>
+                        </i>饰品礼品
                     </dt>
                     <dd>
-                        <a href="/l51/48" target="_blank">节日礼品</a>
-                        <a href="/l51/46" target="_blank">工艺礼品</a>
-                        <a href="/l51/45" target="_blank">女生饰品</a>
-                        <a href="/l51/84" target="_blank">银饰品</a>
-                    </dd>
+                        <a href="/category/48" target="_blank">节日礼品</a>
+                        <a href="/category/46" target="_blank">工艺礼品</a>
+                        <a href="/category/45" target="_blank">女生饰品</a>
+                        <a href="/category/84" target="_blank">银饰品</a></dd>
                 </dl>
                 <dl>
                     <dt>
-                        <i></i>服装加盟
+                        <i>
+                        </i>服装加盟
                     </dt>
                     <dd>
-                        <a href="/l51/51" target="_blank">女装加盟</a>
-                        <a href="/l51/50" target="_blank">男装加盟</a>
-                        <a href="/l51/11" target="_blank">童装加盟</a>
-                        <a href="/l51/53" target="_blank">内衣加盟</a>
-                    </dd>
+                        <a href="/category/51" target="_blank">女装加盟</a>
+                        <a href="/category/50" target="_blank">男装加盟</a>
+                        <a href="/category/11" target="_blank">童装加盟</a>
+                        <a href="/category/53" target="_blank">内衣加盟</a></dd>
                 </dl>
                 <dl>
                     <dt>
-                        <i></i>建材加盟
+                        <i>
+                        </i>建材加盟
                     </dt>
                     <dd>
-                        <a href="/l51/66" target="_blank">家居日用</a>
-                        <a href="/l51/61" target="_blank">灯具灯饰</a>
-                        <a href="/l51/65" target="_blank">液体壁纸</a>
-                        <a href="/l51/59" target="_blank">装饰装修</a>
-                    </dd>
+                        <a href="/category/66" target="_blank">家居日用</a>
+                        <a href="/category/61" target="_blank">灯具灯饰</a>
+                        <a href="/category/65" target="_blank">液体壁纸</a>
+                        <a href="/category/59" target="_blank">装饰装修</a></dd>
                 </dl>
                 <dl>
                     <dt>
-                        <i></i>环保机械
+                        <i>
+                        </i>环保机械
                     </dt>
                     <dd>
-                        <a href="/l51/100" target="_blank">食品机械</a>
-                        <a href="/l51/14" target="_blank">环保材料</a>
-                        <a href="/l51/15" target="_blank">节能设备</a>
-                        <a href="/l51/99" target="_blank">榨油机</a>
-                    </dd>
+                        <a href="/category/100" target="_blank">食品机械</a>
+                        <a href="/category/14" target="_blank">环保材料</a>
+                        <a href="/category/15" target="_blank">节能设备</a>
+                        <a href="/category/99" target="_blank">榨油机</a></dd>
                 </dl>
                 <dl>
                     <dt>
-                        <i></i>教育加盟
+                        <i>
+                        </i>教育加盟
                     </dt>
                     <dd>
-                        <a href="/l51/25" target="_blank">教育培训</a>
-                        <a href="/l51/22" target="_blank">幼儿教育</a>
-                        <a href="/l51/23" target="_blank">益智玩具</a>
-                        <a href="/l51/81" target="_blank">儿童乐园</a>
-                    </dd>
+                        <a href="/category/25" target="_blank">教育培训</a>
+                        <a href="/category/22" target="_blank">幼儿教育</a>
+                        <a href="/category/23" target="_blank">益智玩具</a>
+                        <a href="/category/81" target="_blank">儿童乐园</a></dd>
                 </dl>
                 <dl>
                     <dt>
-                        <i></i>保健加盟
+                        <i>
+                        </i>保健加盟
                     </dt>
                     <dd>
-                        <a href="/l51/32" target="_blank">成人保健</a>
-                        <a href="/l51/34" target="_blank">视力保健</a>
-                        <a href="/l51/30" target="_blank">保健仪器</a>
-                        <a href="/l51/29" target="_blank">保健用品</a>
-                    </dd>
+                        <a href="/category/32" target="_blank">成人保健</a>
+                        <a href="/category/34" target="_blank">视力保健</a>
+                        <a href="/category/30" target="_blank">保健仪器</a>
+                        <a href="/category/29" target="_blank">保健用品</a></dd>
                 </dl>
                 <dl>
                     <dt>
-                        <i></i>美容美体
+                        <i>
+                        </i>美容美体
                     </dt>
                     <dd>
-                        <a href="/l51/79" target="_blank">产后修复</a>
-                        <a href="/l51/31" target="_blank">瘦身减肥</a>
-                        <a href="/l51/26" target="_blank">美容美甲</a>
-                        <a href="/l51/27" target="_blank">化妆品</a>
-                    </dd>
+                        <a href="/category/79" target="_blank">产后修复</a>
+                        <a href="/category/31" target="_blank">瘦身减肥</a>
+                        <a href="/category/26" target="_blank">美容美甲</a>
+                        <a href="/category/27" target="_blank">化妆品</a></dd>
                 </dl>
                 <dl>
                     <dt>
-                        <i></i>农村致富
+                        <i>
+                        </i>农村致富
                     </dt>
                     <dd>
-                        <a href="/l51/71" target="_blank">农村种植</a>
-                        <a href="/l51/69" target="_blank">虫草种植</a>
-                        <a href="/l51/77" target="_blank">布艺加工</a>
-                        <a href="/l51/62" target="_blank">蝎子养殖</a>
-                    </dd>
+                        <a href="/category/71" target="_blank">农村种植</a>
+                        <a href="/category/69" target="_blank">虫草种植</a>
+                        <a href="/category/77" target="_blank">布艺加工</a>
+                        <a href="/category/62" target="_blank">蝎子养殖</a></dd>
                 </dl>
             </div>
         </div>
