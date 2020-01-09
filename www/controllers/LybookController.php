45b88b3bad81a16f5cc0c91130bbe8944301c4e5
wuwulong <1191540273@qq.com>
2020-01-09 11:17:22 +0800

    测试4

--- www/controllers/LybookController.php
+++ www/controllers/LybookController.php
@@ -23,7 +23,7 @@ class LybookController extends BaseWebController
         $info = ApiRequestService::sendPostRequest('/lianzhan/lybook/index',[
             "code" => $code
         ]);
-        var_dump($code);
+        var_dump($info);
         if (!$info) {
             return $this->render('/error/index', ['msg' => ApiRequestService::getLastErrorMsg()]);
         }
