8b18c99294d7b6bc58ed27cfe13c56af075cc6d1
郭威 <apanly@163.com>
2020-03-02 21:00:34 +0800

    guowei -- cache config

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -5,8 +5,9 @@ return [
             'class' => 'common\components\redis\RedisCache',
             'redis' => [
                 'database' => 0,
-                'host'     => '127.0.0.1',
+                'host'     => 'r-wz9upztf8livi4vcn2.redis.rds.aliyuncs.com',
                 'port'     => 6379,
+                'password' => 'xqYlAcF9CvqC1U4i'
                 'prefix'   => 'hsh_page_',
             ]
         ],
